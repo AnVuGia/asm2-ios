43c7773b69dca4955849670f61fb3f8d8dda3a82
Vu Gia An <Vu Gia An>
2023-08-18 12:22:43 +0700

    Initial Commit

---
+++ asm2-ios/asm2_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  asm2_iosApp.swift
+//  asm2-ios
+//
+//  Created by An Vu Gia on 18/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct asm2_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
